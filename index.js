4686afd367f03a5402fdf17a84a76b3aaf592ac0
Gavin Kratzke <gjkkratzke@gmail.com>
2018-10-26 18:51:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,16 +9,17 @@ function takeANumber(katzDeli, name) {
 }
 
 function nowServing(katzDeliLine) {
-if (!katzDeliLine.length) {
-  console.log("There is nobody waiting to be served!")
-  return "There is nobody waiting to be served!"
-} else {
-  return "Currently serving " + katzDeliLine.shift() + "."
-}
-}
-
-function currentLine(katzDeliLine) {
-  if (!katzDeliLine.length) {
-    return "The line is currently empty."
+  var lineNameandNumber = []
+    if (!katzDeliLine.length) {
+      console.log("There is nobody waiting to be served!")
+        return "There is nobody waiting to be served!"
+  } else {
+    return "Currently serving " + katzDeliLine.shift() + "."
   }
 }
+
+    function currentLine(katzDeliLine) {
+      if (!katzDeliLine.length) {
+        return "The line is currently empty."
+      }
+    }
